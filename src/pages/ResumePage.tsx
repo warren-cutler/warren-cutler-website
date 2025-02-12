f4d84fb6460d31d81267e6986bb6106b4afd76d6
warren-cutler <warrenecutler@gmail.com>
2025-02-12 15:36:39 -0500

    convert: JobCard to generic Cards

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -3,7 +3,7 @@ import { Job } from '../types';
 //data
 import { jobsData } from '../data/jobsData';
 //components
-import { JobCard } from '../components/JobCard';
+import { RoleCard } from '../components/RoleCard';
 import { Button } from '../components/generics/Button';
 import { Card } from '../components/generics/Card';
 
@@ -11,9 +11,27 @@ export const ResumePage = () => {
   const jobs: Job[] = jobsData;
 
   return (
-    <Card orientation='col'>
-      {jobs.map((job, idx) => {
-        return <JobCard key={'Job-' + idx} {...job}></JobCard>;
+    <div className='flex flex-col px-5 m-5 py-1 justify-start min-w-sm'>
+      {jobs.map((job) => {
+        return (
+          <Card orientation='col'>
+            <div className='flex items-center justify-between'>
+              <a href={job.companyUrl} target='_blank'>
+                <img
+                  src={job.companyLogo}
+                  height='50'
+                  width='50'
+                  alt='Click to visit the employer website'
+                />
+              </a>
+              <p>{job.company}</p>
+              <p>{job.location}</p>
+            </div>
+            {job.roles.map((role, idx) => {
+              return <RoleCard key={'Role-' + idx} {...role}></RoleCard>;
+            })}
+          </Card>
+        );
       })}
       <div className='self-center'>
         <a href='/src/assets/cutler_resume.pdf' target='_blank'>
@@ -22,6 +40,6 @@ export const ResumePage = () => {
           </Button>
         </a>
       </div>
-    </Card>
+    </div>
   );
 };
