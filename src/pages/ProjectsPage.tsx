66a61d27567415596b042164f1b5cba8e0456e88
warren-cutler <warrenecutler@gmail.com>
2025-02-13 17:55:56 -0500

    styling: Projects Page / project cards

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -9,15 +9,23 @@ export const ProjectsPage = () => {
   const projects: Project[] = projectsData;
 
   return (
-    <div className='grid grid-cols-[repeat(auto-fill,minmax(384px,1fr))] px-5 m-5 py-1 gap-[2vw] justify-center min-w-sm'>
-      {projects.map((project) => {
+    <div className='self-stretch grid grid-cols-[repeat(auto-fill,minmax(384px,1fr))] px-5 m-5 py-1 gap-[2vw] justify-center min-w-sm'>
+      {projects.map((project, idx) => {
         return (
-          <Card >
+          <Card key={idx} className='flex flex-col items-center p-2 max-w-xl'>
             <img src={project.imagePath} height='200' width='200'></img>
-            <p>{project.name}</p>
-            <p>{project.description}</p>
-            <p>{project.linkUrl ? project.linkUrl : ''}</p>
-            <p>{project.status}</p>
+            <h2>{project.name}</h2>
+            <p className='text-center'>{project.description}</p>
+            {project.linkUrl ? (
+              <a className='underline' href={project.linkUrl}>
+                {project.linkUrl}
+              </a>
+            ) : (
+              ''
+            )}
+            <div className='flex grow self-end'>
+              <p className='italic self-end'>{project.status}</p>
+            </div>
           </Card>
         );
       })}
