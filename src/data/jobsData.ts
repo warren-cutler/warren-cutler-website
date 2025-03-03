b5a15413b7f55aff60361d76dc96c3e5325a312a
warren-cutler <warrenecutler@gmail.com>
2025-03-03 14:17:22 -0500

    update: Resume page/PDF to match March resume rewrite

--- src/data/jobsData.ts
+++ src/data/jobsData.ts
@@ -5,8 +5,9 @@ const senseRole: Role = {
   dateStart: new Date("2022-09-01"),
   dateEnd: new Date("2024-09-01"),
   details: [
-    "Spearheaded production line design of a dual ethernet version of energy monitor brought to market, including determining test software and test head UI requirements, analyzing build results to refine testing thresholds, and overseeing the deployment, training, and load balancing at a contract manufacturer site.",
-    "Planned, deployed, and managed an RMA process at a third party partner, implementing automated functional testing through proprietary software to reclaim product inventory and reduce electronic waste.",
+    "Spearheaded production line design of an energy monitor brought to market, determining test software thresholds and UI requirements to simplify operator workflows, load balance efficiently, and prevent quality escapes.",
+    "Designed, deployed, and managed an RMA process at a third party partner, implementing automated functional testing through proprietary software to enable product reclamation and reduce electronic waste by up to 75%.",
+    "Conducted extensive regression testing for monthly software updates, utilizing automated test tools and scripts, to identify and flag performance issues in embedded systems and maintain backwards compatibility of field-deployed legacy devices.",
   ],
 };
 
@@ -15,18 +16,20 @@ const cognexNPIRole: Role = {
   dateStart: new Date("2021-02-01"),
   dateEnd: new Date("2022-09-01"),
   details: [
-    "Managed alpha (in-house), beta (CM), and RTM production of machine vision systems, including designing line configuration, training operators, debugging assembly issues, resolving test issues, and updating key stakeholders.",
-    "Headed Design for Assembly reviews with cross-team engineers for alpha stage projects to improve hardware standardization, simplify processes, plan appropriate test coverage, and leverage past engineering efforts.",
+    "Managed alpha (in-house), beta (CM), and RTM production of machine vision systems, including designing line configuration, training operators, debugging assembly and design issues, resolving test issues, and updating key stakeholders.",
+    "Conducted Design for Assembly exercises with cross-team engineers to improve alpha-stage design standardization, simplify assemblies, plan appropriate test coverage, and leverage past engineering efforts to create a smoother design release process.",
+    "Led mentorship sessions with junior engineers to facilitate the resolution of production bottlenecks, employing root cause analysis and data-driven decision-making processes.",
     "Designed assembly fixtures in SolidWorks and fabricated them using in-house 3D printing equipment for use in mass production of high-volume machine vision systems, ensuring production capacity and quality met KPIs.",
   ],
 };
+
 const cognexSustainingRole: Role = {
   title: "Associate Manufacturing Engineer, Sustaining Engineering",
   dateStart: new Date("2018-05-01"),
   dateEnd: new Date("2021-02-01"),
   details: [
-    "Coordinated with software and electrical engineers to build three types of modular battery charging stations, developed the mechanical design and fabrication, ensuring international shipping regulation compliance.",
-    "Organized a swift release process to accelerate manufacturing of over forty customer-specific products in the Life Sciences, coordinating with test and software engineers to identify key requirements and implement production environment testing.",
+    "Designed, fabricated, and assembled custom modular battery charging/discharging stations, prioritizing ease of use and field repairability, ensuring global distribution centers adhered to lithium battery shipping regulations.",
+    "Organized a swift release process to accelerate development of over forty products in the Life Sciences, coordinating with software engineers to implement production environment testing according to stringent customer-specific requirements.",
   ],
 };
 
